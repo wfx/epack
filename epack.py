272df45eefae733c871bd26502473acc27697ca0
wfx <wolfgang.morawetz@gmail.com>
2014-12-01 21:43:21 +0100

    change licenses to GPL3 and some cosmetic

--- epack.py
+++ epack.py
@@ -1,29 +1,44 @@
 #!/usr/bin/env python
 # encoding: utf-8
 #
-# Copyright (c) 2013, Wolfgang Morawetz (wfx).
-# All rights reserved.
-# Use of this source code is governed by a BSD-style license that can be
-# found in the LICENSE file.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 __author__ = "Wolfgang Morawetz"
-__version__ = "Alpha.0.2014.11.30"
+__copyright__ = "Copyright (C) 2014 Wolfgang Morawetz"
+__version__ = "Alpha.0.2014.12.1"
+__description__ = 'A simple tool to extract any file with efm'
+__github__ = 'https://github.com/wfx/epack'
+__source__ = 'Source code and bug reports: {0}'.format(__github__)
+PY_EFL = "https://git.enlightenment.org/bindings/python/python-efl.git/"
 
 import os
 import sys
 import magic
-
-from efl.evas import EVAS_HINT_EXPAND, EVAS_HINT_FILL
-from efl import elementary
-from efl.elementary.window import StandardWindow
-from efl.elementary.box import Box
-from efl.elementary.frame import Frame
-from efl.elementary.icon import Icon
-from efl.elementary.label import Label
-from efl.elementary.list import List
-from efl.elementary.button import Button
-from efl.elementary.panel import Panel, ELM_PANEL_ORIENT_LEFT
+try:
+	from efl.evas import EVAS_HINT_EXPAND, EVAS_HINT_FILL
+	from efl import elementary
+	from efl.elementary.window import StandardWindow
+	from efl.elementary.box import Box
+	from efl.elementary.frame import Frame
+	from efl.elementary.icon import Icon
+	from efl.elementary.label import Label
+	from efl.elementary.list import List
+	from efl.elementary.button import Button
+	from efl.elementary.panel import Panel, ELM_PANEL_ORIENT_LEFT
+except ImportError:
+	printErr("ImportError: Please install Python-EFL:\n ", PY_EFL)
+	exit(1)
 
 EXPAND_BOTH = EVAS_HINT_EXPAND, EVAS_HINT_EXPAND
 EXPAND_HORIZ = EVAS_HINT_EXPAND, 0.0
@@ -42,8 +57,8 @@ class Application(object):
 		self.win = None
 		self.bx_main = None
 
-		self.win = StandardWindow("my app", "enpack", focus_highlight_enabled=True, autodel=True)
-		self.win.title_set("enpack")
+		self.win = StandardWindow("my app", "epack", focus_highlight_enabled=True, autodel=True)
+		self.win.title_set("epack")
 		self.win.callback_delete_request_add(self.destroy)
 
 		self.bx_main = Box(self.win, size_hint_weight=EXPAND_BOTH)
