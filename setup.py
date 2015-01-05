6f3054df573b6ef1c662f6c6dcea1824b321fe92
Dave Andreoli <dave@gurumeditation.it>
2015-01-05 19:12:01 +0100

    Use the the distutils Command build_fdo provided by pyefl
    Now .desktop file and icons are installed automatically.
    Also changed a bit the layout of icons to better suite
    the fdo standard (category folder added)

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 from distutils.core import setup
-from efl.utils.setup import build_extra, build_i18n, uninstall
+from efl.utils.setup import build_extra, build_i18n, build_fdo, uninstall
 
 
 setup(
@@ -15,18 +15,10 @@ setup(
     provides = ['epack'],
     packages = ['epack', 'epack.libarchive'],
     scripts = ['bin/epack'],
-    data_files = [
-        ('share/applications', ['data/epack.desktop']),
-        ('share/icons', ['data/epack.png']),
-        ('share/icons/hicolor/24x24/apps', ['data/icons/24x24/epack.png']),
-        ('share/icons/hicolor/32x32/apps', ['data/icons/32x32/epack.png']),
-        ('share/icons/hicolor/64x64/apps', ['data/icons/64x64/epack.png']),
-        ('share/icons/hicolor/128x128/apps', ['data/icons/128x128/epack.png']),
-        ('share/icons/hicolor/256x256/apps', ['data/icons/256x256/epack.png']),
-    ],
     cmdclass = {
         'build': build_extra,
         'build_i18n': build_i18n,
+        'build_fdo': build_fdo,
         'uninstall': uninstall,
     },
     command_options={
